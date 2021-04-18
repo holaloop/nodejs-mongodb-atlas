663b76a63ce89c063b095f8425e95a9e8714fa01
holaloop <81982652+holaloop@users.noreply.github.com>
2021-04-18 02:40:52 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
 const mongodb = require('mongodb');
 
 var MongoClient = require('mongodb').MongoClient;
-var uri = "YOUR_URL";
+var uri = "mongodb+srv://admin:rnuuIkHIQPpESjFB@cluster0.ntvqy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 MongoClient.connect(uri, function (err, db) {
 //db.close();
 if (err) {
